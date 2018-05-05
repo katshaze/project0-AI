0e69a60a4988b8ed7fa2ddab6476b858730f7e33
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-05-05 10:43:05 +1000

    transpiled via babel

--- src/logic.js
+++ src/logic.js
@@ -1,8 +1,6 @@
-"use strict";
-
 // AI (Easy) Version: code for state of play
 
-var game = {
+const game = {
 
   startingPlayer: "X",
 
@@ -42,7 +40,7 @@ var game = {
     "Blowfish": 0
   },
 
-  playTurn: function playTurn(square, player) {
+  playTurn: function(square, player) {
     if (this.boardStatus[square] === "empty") {
       this.boardStatus[square] = player;
       this.turnsPlayed[player] += 1;
@@ -58,53 +56,53 @@ var game = {
   },
 
   //function to randomly choose a square for the computer to play in based on what's available. gets called from presentation.js at same time as running the playturn function for the computer.
-  chooseSquareAI: function chooseSquareAI() {
-    var availableSquares = [];
-    for (var key in this.boardStatus) {
+  chooseSquareAI: function() {
+    let availableSquares = [];
+    for (let key in this.boardStatus) {
       if (this.boardStatus[key] === "empty") {
         availableSquares.push(key);
       }
     }
-    var chosenSquare = availableSquares[Math.floor(Math.random() * availableSquares.length)];
+    let chosenSquare = availableSquares[Math.floor(Math.random() * availableSquares.length)];
     return chosenSquare;
   },
 
-  checkForWin: function checkForWin(player) {
+  checkForWin: function(player) {
     if (this.boardStatus[1] === player && this.boardStatus[2] === player && this.boardStatus[3] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [1, 2, 3];
+      this.winningStrip = [1,2,3];
     }
     if (this.boardStatus[1] === player && this.boardStatus[5] === player && this.boardStatus[9] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [1, 5, 9];
+      this.winningStrip = [1,5,9];
     }
     if (this.boardStatus[1] === player && this.boardStatus[4] === player && this.boardStatus[7] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [1, 4, 7];
+      this.winningStrip = [1,4,7];
     }
     if (this.boardStatus[2] === player && this.boardStatus[5] === player && this.boardStatus[8] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [2, 5, 8];
+      this.winningStrip = [2,5,8];
     }
     if (this.boardStatus[3] === player && this.boardStatus[6] === player && this.boardStatus[9] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [3, 6, 9];
+      this.winningStrip = [3,6,9];
     }
     if (this.boardStatus[3] === player && this.boardStatus[5] === player && this.boardStatus[7] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [3, 5, 7];
+      this.winningStrip = [3,5,7];
     }
     if (this.boardStatus[4] === player && this.boardStatus[5] === player && this.boardStatus[6] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [4, 5, 6];
+      this.winningStrip = [4,5,6];
     }
     if (this.boardStatus[7] === player && this.boardStatus[8] === player && this.boardStatus[9] === player) {
       this.winningCombo[player] = true;
-      this.winningStrip = [7, 8, 9];
+      this.winningStrip = [7,8,9];
     }
   },
 
-  checkForDraw: function checkForDraw() {
+  checkForDraw: function() {
     if (this.turnsPlayed["X"] + this.turnsPlayed["Blowfish"] === 9) {
       if (this.winningCombo["X"] === false && this.winningCombo["Blowfish"] === false) {
         this.winningCombo["Draw"] = true;
@@ -113,15 +111,15 @@ var game = {
     }
   },
 
-  checkForEndgame: function checkForEndgame() {
-    for (var key in this.winningCombo) {
+  checkForEndgame: function() {
+    for (let key in this.winningCombo) {
       if (this.winningCombo[key] === true) {
         this.endgame = true;
       }
     }
   },
 
-  updateCurrentPlayer: function updateCurrentPlayer(player) {
+  updateCurrentPlayer: function(player) {
     if (player === "X") {
       this.currentPlayer = "Blowfish";
     } else {
--- src/presentation.js
+++ src/presentation.js
@@ -1,18 +1,16 @@
-'use strict';
-
 // AI (Easy) version: code for interaction with the browser
 
-$(document).ready(function () {
+$(document).ready(function() {
   reset(); //this will ensure that on refresh, the starting player will be blowfish, so that the human can start first game.
 
   // event listener for click to reset in endgame situation.
-  $('body').on('click', function () {
+  $('body').on('click', function() {
     if (game.endgame === true) {
       reset();
     }
   });
 
-  $('.square').on('click', function (event) {
+  $('.square').on('click', function(event) {
     // if the last click caused an endgame, exit out of here instead of running the player's turn
     if (game.endgame === true) {
       return;
@@ -20,7 +18,7 @@ $(document).ready(function () {
     // endgame is false if we get to here. therefore, turn off the endgame event listener by using stop propogation for now since it's not an endgame situation.
     event.stopPropagation();
 
-    var square = $(this).attr("id"); //get the square name
+    const square = $(this).attr("id"); //get the square name
     if (game.currentPlayer === "Blowfish") {
       game.playTurn(square, game.currentPlayer);
       render();
@@ -28,7 +26,7 @@ $(document).ready(function () {
 
     if (game.endgame != true) {
       if (game.currentPlayer === "X") {
-        setTimeout(function () {
+        setTimeout(function() {
           game.playTurn(game.chooseSquareAI(), game.currentPlayer);
           render();
         }, 500);
@@ -39,7 +37,7 @@ $(document).ready(function () {
   $('#reset').on('click', reset);
 });
 
-var reset = function reset() {
+const reset = function() {
 
   //switch starting player from what it was at start of last game
   if (game.startingPlayer === "X") {
@@ -76,61 +74,62 @@ var reset = function reset() {
   newGameRender();
 };
 
-var newGameRender = function newGameRender() {
+const newGameRender = function() {
   $('.visible').removeClass('visible');
   $('.makeBig').removeClass('makeBig');
   $('.flash').removeClass('animated flash');
-  $('.' + game.startingPlayer + '-starts').addClass('visible');
+  $(`.${game.startingPlayer}-starts`).addClass('visible');
 
   //if it's start of a new game, board is reset and starting player is X (computer), play turn function is called for the computer.
   if (game.currentPlayer === "X") {
-    setTimeout(function () {
+    setTimeout(function() {
       game.playTurn(game.chooseSquareAI(), game.currentPlayer);
       render();
     }, 300);
   }
 };
 
-var render = function render() {
+const render = function() {
 
   // remove the msg re who starts the game if more than one move has been made by any player
-  for (var key in game.turnsPlayed) {
+  for (let key in game.turnsPlayed) {
     if (game.turnsPlayed[key] > 1) {
-      $('.' + key + '-starts').removeClass('visible');
+      $(`.${key}-starts`).removeClass('visible');
     }
   }
 
   // update the board squares with wherever X/Blowfish have played.
-  for (var _key in game.boardStatus) {
-    if (game.boardStatus[_key] === "X") {
-      $('#' + _key + ' .x').addClass('visible');
+  for (let key in game.boardStatus) {
+    if (game.boardStatus[key] === "X") {
+      $(`#${key} .x`).addClass('visible');
     }
-    if (game.boardStatus[_key] === "Blowfish") {
-      $('#' + _key + ' .blowfish').addClass('visible');
+    if (game.boardStatus[key] === "Blowfish") {
+      $(`#${key} .blowfish`).addClass('visible');
     }
   }
 
   // if winningCombo[X/Blowfish/Draw] is true, make text appear at bottom saying X/Blowfish/Draw Wins (simple mode)
-  for (var _key2 in game.winningCombo) {
-    if (game.winningCombo[_key2] === true) {
-      $('.' + _key2 + '-wins').addClass('visible');
+  for (let key in game.winningCombo) {
+    if (game.winningCombo[key] === true) {
+      $(`.${key}-wins`).addClass('visible');
     }
   }
 
   // The blowfish puffs up if it wins.
   if (game.winningCombo["Blowfish"] === true) {
-    $('#' + game.winningSquare + '\n     .blowfish').addClass('makeBig');
+    $(`#${game.winningSquare}
+     .blowfish`).addClass('makeBig');
   }
 
   // The three winning Xs flash if X wins.
   if (game.winningCombo["X"] === true) {
-    $('#' + game.winningStrip[0] + ' .x').addClass('animated flash');
-    $('#' + game.winningStrip[1] + ' .x').addClass('animated flash');
-    $('#' + game.winningStrip[2] + ' .x').addClass('animated flash');
+    $(`#${game.winningStrip[0]} .x`).addClass('animated flash');
+    $(`#${game.winningStrip[1]} .x`).addClass('animated flash');
+    $(`#${game.winningStrip[2]} .x`).addClass('animated flash');
   }
 
   // The win gets added to the relevant tally
-  for (var _key3 in game.winsTally) {
-    $('.' + _key3 + '-tally').html('' + game.winsTally[_key3]);
+  for (let key in game.winsTally) {
+    $(`.${key}-tally`).html(`${game.winsTally[key]}`);
   }
 };
